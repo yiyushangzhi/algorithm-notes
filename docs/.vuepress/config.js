391c3b3a689b67ce83e1a47ec336feb9d85a6186
litezhi <909000590@qq.com>
2021-05-31 20:03:01 +0800

    project initialize

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Algorithm Note',
+  description: 'My algorithm note.',
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: [
+    '@vuepress/active-header-links',
+    '@vuepress/back-to-top',
+  ]
+};
---
+++ docs/README.md
@@ -0,0 +1,1 @@
+[单调栈](./monotonic-stack.md)
